7f2bcb8be15a572b3b89bc7251dbdf66c66a8178
Lucas <garcialj97@gmail.com>
2022-11-21 21:52:48 -0300

    contact-form

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/********************************
+             NAVBAR
+********************************/
+const navBar = document.getElementById("nav__bar");
+const buttonClose = document.getElementById("nav__button--close");
+const buttonOpen = document.getElementById("nav__button--open");
+
+buttonClose.addEventListener("click", closeNav = (e) => {
+        e.preventDefault();
+        navBar.style.width = "0%";
+});
+
+buttonOpen.addEventListener("click", openNav = (e) => {
+        e.preventDefault();
+        navBar.style.width = "100%";
+});
